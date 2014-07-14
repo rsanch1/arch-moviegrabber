a2b005006f3666349a1029c4878078493d4414f8
Robert Sanchez <rsanch1@gmail.com>
2014-07-14 18:49:29 -0500

    Update moviegrabber.sh

--- moviegrabber.sh
+++ moviegrabber.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /sbin/setuser nobody python /opt/moviegrabber/moviegrabber-master/MovieGrabber.py --config /config/config --db /config/db --logs /config/logs
+exec python /opt/moviegrabber/moviegrabber-master/MovieGrabber.py --config /config/config --db /config/db --logs /config/logs
